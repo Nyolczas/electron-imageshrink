c8e176ca0c4972535f0f42728d63ed13cbbb7feb
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-12-03 17:35:01 +0100

    loading index.html

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 const { app, BrowserWindow } = require('electron');
 
+// Set env
+process.env.NODE_ENV = 'development';
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+
+let mainWindow;
+
 const createMainWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/icon.ico`,
   });
+
+  //mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile('./app/index.html');
 };
 
 app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
