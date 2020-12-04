e4892e9c7e70e57d1925e242ab4be7f677d24fd6
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-12-04 21:19:12 +0100

    Frontend

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // Set env
 process.env.NODE_ENV = 'development';
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 const createMainWindow = () => {
   mainWindow = new BrowserWindow({
@@ -17,27 +18,52 @@ const createMainWindow = () => {
   //mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   mainWindow.loadFile('./app/index.html');
 };
+const createAboutWindow = () => {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    resizable: false,
+    icon: `${__dirname}/assets/icons/icon.ico`,
+  });
+
+  aboutWindow.loadFile('./app/about.html');
+};
 
 app.on('ready', () => {
   createMainWindow();
 
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
-
-  globalShortcut.register('Ctrl+R', () => mainWindow.reload());
-  globalShortcut.register('Ctrl+Shift+I', () => mainWindow.toggleDevTools());
-
   mainWindow.on('closed', () => (mainWindow = null));
 });
 
 const menu = [
   {
-    label: 'Fájl',
+    role: 'fileMenu',
+  },
+  {
+    role: 'editMenu',
+  },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+  {
+    label: 'Help',
     submenu: [
       {
-        label: 'Quit',
-        accelerator: 'Ctrl+W',
-        click: () => app.quit(),
+        label: 'About',
+        click: createAboutWindow,
       },
     ],
   },
